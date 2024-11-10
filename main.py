15631bb0245a0766301f2c021443c804cbb261b3
StarHawk <goubin.a@mail.ru>
2024-11-10 12:49:20 +0300

    line now in file, fixed empty X, A1 now have usage

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import re
 from contextlib import redirect_stdout
 
-#TODO: не только единицы должны быть, но и всё из А1
-def build_regex(pattern_str, variables):
-    """
-    Строит регулярное выражение из строки с переменными, заменяя переменные на '1+'.
-    """
-    escaped_pattern = re.escape(pattern_str)
 
+def build_regex(pattern_str, variables, allowed_symbols):
+    escaped_pattern = re.escape(pattern_str)
+    symbol_class = ''.join(re.escape(char) for char in allowed_symbols)
     for variable in variables:
-        # Заменяем первое вхождение переменной на именованную группу
-        group_pattern = f"(?P<{variable}>1+)"
+        # Заменяем первое вхождение переменной на именованную группу, захватывающую символы из allowed_symbols
+        group_pattern = f"(?P<{variable}>[{symbol_class}]+)"
         escaped_pattern = re.sub(rf"(?<!<)\b{re.escape(variable)}\b", group_pattern, escaped_pattern, count=1)
+
         # Заменяем все последующие вхождения переменной на ссылку на группу
         reference_pattern = f"(?P={variable})"
         escaped_pattern = re.sub(rf"(?<!<)\b{re.escape(variable)}\b", reference_pattern, escaped_pattern)
@@ -60,10 +58,15 @@ variables = {}
 # Находим все совпадения в тексте
 matches = re.findall(pattern, data)
 
-# Заполняем словарь значениями без внешних кавычек
+# Заполняем словарь значениями
 for name, value in matches:
     variables[name] = value
 
+is_line = re.search(r'line\s*=\s*(.+)', data)
+if is_line:
+    line = is_line.group(1).strip()
+else:
+    line = ''
 # Пример вывода полученных значений
 print(variables)
 
@@ -71,13 +74,8 @@ R = variables['R'].split(',')
 A = variables['A'].split(',')
 X = variables['X'].split(',')
 A1 = variables['A1'].split(',')
-#line = '111111111111111111111/1111='
-line = '11111+111111111/111='
 
-for i in line:
-    if i not in A:
-        print('Строка не принадлежит алфавиту A')
-        exit(0)
+
 
 print('A:', A)
 print('X:', X)
@@ -86,28 +84,44 @@ print('R:', R)
 
 with open('output.txt', 'w', encoding='utf8') as f:
     with redirect_stdout(f):
+
         print(line)
+        for i in line:
+            if i not in A:
+                print('Строка не принадлежит алфавиту A')
+                exit(0)
         print('-' * 30)
         i = 0
         while True:
             while i < len(R):
                 was, become = R[i].split('->')
-                was_regex = build_regex(was, X)
+                if X!=['']:
+                    was_regex = build_regex(was, X, A1)
+                    # Проверяем, есть ли переменные в строке 'become'
+                    extracted_vars = extract_variables(was_regex, line, X)
 
-                # Проверяем, есть ли переменные в строке 'become'
-                extracted_vars = extract_variables(was_regex, line, X)
+                    if extracted_vars:  # Если переменные есть, выполняем основную логику
+                        become = substitute_variables(become, extracted_vars)
+                        was = substitute_variables(was, extracted_vars)
+                        if re.search(was_regex, line):
 
-                if extracted_vars:  # Если переменные есть, выполняем основную логику
-                    become = substitute_variables(become, extracted_vars)
-                    was = substitute_variables(was, extracted_vars)
-                    if re.search(was_regex, line):
-                        i = 0
-                        line = line.replace(was, become, 1)
-                        print(line)
-                        print(R[i])
-                        print('-' * 30)
+                            line = line.replace(was, become, 1)
+                            print(line)
+                            print(R[i])
+                            print('-' * 30)
+                            i = 0
+                        else:
+                            i += 1
+
                     else:
-                        i += 1
+                        if re.search(was, line):
+                            i = 0
+                            line = re.sub(was, become, line)
+                            print(line)
+                            print(R)
+                            print('-' * 30)
+                        else:
+                            i += 1
                 else:
                     if re.search(was, line):
                         i = 0
